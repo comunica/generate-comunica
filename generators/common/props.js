d69dc6500fe112a876967d54d96fab2f8118464f
Jesse Wright <jesse.wright@anu.edu.au>
2020-12-01 00:20:48 +1100

    Fix crash when setting of props['versionBus'] for buses and mediators
    * fix setting of props['versionBus'] for buses and mediators
    
    * Fix condition to determin if busName is defined
    
    * Remove namespace param
    
    * Removing namespace param from populateProps

--- generators/common/props.js
+++ generators/common/props.js
@@ -4,7 +4,9 @@ function populateProps(props, basePath) {
   var cwd = process.cwd() + '/';
   var requirePath = cwd + basePath + '../node_modules/@comunica/';
   props['versionComunicaCore'] = require(requirePath + 'core/package.json').version;
-  props['versionBus'] = require(requirePath + 'bus-' + props.busName + '/package.json').version;
+  if (props.busName) {
+    props['versionBus'] = require(requirePath + 'bus-' + props.busName + '/package.json').version;
+  }
 }
 
 module.exports = {
